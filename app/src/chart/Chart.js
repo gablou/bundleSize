f6c3a70a19fcab1af978700bedb4a9fd792b70d8
gablou <el.gablou@outlook.fr>
2020-09-06 22:29:18 +0200

    chart display

--- app/src/chart/Chart.js
+++ app/src/chart/Chart.js
@@ -1,25 +1,60 @@
 import React from "react";
+import filesize from "filesize.js";
+import "./Chart.css";
+
+function SizeBar(props) {
+  const gzSizeStyle = {
+    height: `${Math.floor((props.sizeGzip / props.max) * 100)}%`,
+  };
+  const sizeStyle = {
+    height: `${Math.floor(((props.size - props.sizeGzip) / props.max) * 100)}%`,
+  };
+  console.log(sizeStyle);
+  return (
+    <div className="bar">
+      <div style={sizeStyle} className="size">
+        <div className="size-label">{filesize(props.size)}</div>
+      </div>
+      <div style={gzSizeStyle} className="gz-size">
+        <div className="size-label">{filesize(props.sizeGzip)}</div>
+      </div>
+    </div>
+  );
+}
 
 function Chart(props) {
-  console.log(props);
   if (props.result.data === null) {
     return null;
   }
   if (props.result.error) {
     return <div>{props.result.data}</div>;
   }
+
   const packageDatas = props.result.data;
+  const maxSize = packageDatas.reduce(function(a, b) {
+    return { size: Math.max(a.size || 0, b.size || 0) };
+  }).size;
   const listItems = packageDatas.map((packageData, index) => (
-    <li key={index}>
-      <div>{packageData.size}</div>
-      <div>{packageData.sizeGzip}</div>
-      <div>{packageData.version}</div>
-    </li>
+    <div key={index}>
+      {packageData.error ? (
+        <div className="bar error">could not calculate</div>
+      ) : (
+        <SizeBar
+          max={maxSize}
+          size={packageData.size}
+          sizeGzip={packageData.sizeGzip}
+        />
+      )}
+      <div className="version">{packageData.version}</div>
+    </div>
   ));
   return (
     <div>
-      <div>{packageDatas[0].packageName}</div>
-      <ul>{listItems}</ul>
+      <div className="d-flex justify-content-center">
+        <span className="minimified">Minimified</span>
+        <span className="gziped">Gziped</span>
+      </div>
+      <div className="chart">{listItems}</div>
     </div>
   );
 }
